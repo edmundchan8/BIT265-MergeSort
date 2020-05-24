80b3732e138d4a4523a867f1f111890c91be7505
edmundchan <pokedev8@gmail.com>
2020-05-24 15:58:14 -0700

    Pushing initial changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,7 +41,6 @@ namespace BIT265_MergeSort.Controllers
                 if (ivm.WifiHours != null)
                 {
                     var wifi = _dbc.WifiHotposts.Where(w => w.City == "EVERETT"));
-
                     wifi = mergeSortServices.MergeSort(_dbc.WifiHotposts.Any());
                     return RedirectToAction("Result", dm);
                 }
